65a347beec0a97eb3be1fabccd6afcb467e90949
evhenious <evhenious@gmail.com>
2024-04-29 14:42:49 +0300

    simplified log level change

--- utils/logger.js
+++ utils/logger.js
@@ -7,34 +7,17 @@ function getLogger(moduleName, loggerConfig) {
         disableColors();
     }
 
-    switch(logLevel) {
-        case 'info':
-            return {
-            info:(...args) => console.log(bgBlue(`${moduleName}:`), ...args),
-            warn:(...args) => console.error(bgYellow(`${moduleName}:`), ...args),
-            error:(...args) => console.error(bgRed(`${moduleName}:`), ...args)
-            }
-            break;
-        case 'warn':
-            return {
-                info:() =>{},
-                warn:(...args) => console.error(bgYellow(`${moduleName}:`), ...args),
-                error:(...args) => console.error(bgRed(`${moduleName}:`), ...args)
-            }
-            break;
-        case 'error':
-            return {
-                info:() =>{},
-                warn:() =>{},
-                error:(...args) => console.error(bgRed(`${moduleName}:`), ...args)
-            }
-        default:
-            return {
-                info:() =>{},
-                warn:(...args) => console.error(bgYellow(`${moduleName}:`), ...args),
-                error:() =>{}
-            }
-            break;
+    return {
+        info:(...args) => {
+            if (logLevel !== 'info') return;
+            console.log(bgBlue(`${moduleName}:`), ...args);
+        },
+        warn:(...args) => {
+            if (logLevel === 'error') return;
+            console.error(bgYellow(`${moduleName}:`), ...args);
+        },
+        // а тут додаткової логіки взагалі не треба, бо метод працює при будь-яких значеннях LOG_LEVEL, правда? :)
+        error:(...args) => console.error(bgRed(`${moduleName}:`), ...args)
     }
 }
 
