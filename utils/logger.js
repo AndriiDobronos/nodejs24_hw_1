f3e773a227a9c5dff71fb26e94c6a41a59d25d46
evhenious <evhenious@gmail.com>
2024-04-30 22:47:10 +0300

    fixed logger

--- utils/logger.js
+++ utils/logger.js
@@ -8,16 +8,17 @@ function getLogger(moduleName, loggerConfig) {
     }
 
     return {
-        info:(...args) => {
+        info: (...args) => {
             if (logLevel !== 'info') return;
             console.log(bgBlue(`${moduleName}:`), ...args);
         },
-        warn:(...args) => {
+        warn: (...args) => {
             if (logLevel === 'error') return;
             console.error(bgYellow(`${moduleName}:`), ...args);
         },
-        // а тут додаткової логіки взагалі не треба, бо метод працює при будь-яких значеннях LOG_LEVEL, правда? :)
-        error:(...args) => console.error(bgRed(`${moduleName}:`), ...args)
+        error: (...args) => {
+            console.error(bgRed(`${moduleName}:`), ...args);
+        }
     }
 }
 
