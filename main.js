1ec663654d0afd2e97ec49e55bbd60cca164248d
Andrii Dobronos <andriidobronos@gmail.com>
2024-05-01 10:16:55 +0300

    changes in main

--- main.js
+++ main.js
@@ -8,14 +8,9 @@ const logger = require('./utils/logger')('main',config );
 logger.info('info message : the script is running!');
 logger.warn('warn message');
 logger.error('error message');
-logger.warn('warn message');
-logger.error('error message');
 
 const { fn } = require('./secondary');
 //fn();
-//fn();
 
-const fileSync = require('./file_sync');
-fileSync.start('source','target');
 const fileSync = require('./file_sync');
 fileSync.start('source','target');
