8b511c291a3dd1f9142089840cc76497acc21c70
Andrii Dobronos <andriidobronos@gmail.com>
2024-05-04 10:51:02 +0300

    removed everything unnecessary

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ logger.info('info message : the script is running!');
 logger.warn('warn message');
 logger.error('error message');
 
-//const { fn } = require('./secondary');
-//fn();
+const { fn } = require('./secondary');
+fn();
 
 const fileSync = require('./file_sync');
 fileSync.start('source','target');
--- utils/logger.js
+++ utils/logger.js
@@ -2,27 +2,24 @@ const { bgBlue, bgYellow, bgRed, disable: disableColors }  = require('colors/saf
 const path = require('path');
 const fs = require('fs');
 
-//*** createLogsFolder був перший варіант для колбека на подію 'init_logger'***
-const createLogsFolder = require('../functions/createLogsFolder.js')
-
 function getLogger(moduleName, loggerConfig) {
     const { logLevel, colorsEnabled, target} = loggerConfig;
     const logsFolderPath = path.join(__dirname, target);
-    const writeStreamInfo = fs.createWriteStream(path.join(logsFolderPath, 'info.log.txt'))
-    const writeStreamError = fs.createWriteStream(path.join(logsFolderPath, 'error.log.txt'))
+    const writeStreamInfo = fs.createWriteStream(path.join(logsFolderPath, 'info.log'))
+    const writeStreamError = fs.createWriteStream(path.join(logsFolderPath, 'error.log'))
     const date = new Date().toLocaleString()
-    process.emit('init_logger')
+
     process.once('init_logger',()=> {
         !fs.existsSync(logsFolderPath) ?
             (fs.mkdirSync(logsFolderPath), console.log(`Папка ${target} була створена`)) :
         console.log(`Папка ${target} вже існує`);
     });
+    process.emit('init_logger')
 
     process.once('beforeExit',()=> {
         writeStreamInfo.end();
         writeStreamError.end();
         console.log('Stream закрився')})
-    process.on('exit',()=>console.log('finish'))
 
     if (+colorsEnabled === 0) {
         disableColors();
@@ -30,26 +27,16 @@ function getLogger(moduleName, loggerConfig) {
 
     return {
         info: (...args) => {
-            //Якщо треба зберігати усю історію info для усіх викликів logger:
-            //fs.appendFileSync((path.join(logsFolderPath, 'info.log.txt')),`${date} : ${moduleName} : ${[...args]}\n`)
-
             writeStreamInfo.write(`${date} : ${moduleName} : ${[...args]}\n`)
             if (logLevel !== 'info') return;
             console.log(bgBlue(`${moduleName}:`), ...args);
-
         },
         warn: (...args) => {
-            //Якщо треба зберігати історію з усіх викликів logger:
-            //fs.appendFileSync((path.join(logsFolderPath, 'error.log.txt')),`${date} : ${moduleName} : ${[...args]}\n`)
-
             writeStreamError.write(`${date} : ${moduleName} : ${[...args]}\n`)
             if (logLevel === 'error') return;
             console.error(bgYellow(`${moduleName}:`), ...args);
         },
         error: (...args) => {
-            //Якщо треба зберігати історію з усіх викликів logger:
-            //fs.appendFileSync((path.join(logsFolderPath, 'error.log.txt')),`${date} : ${moduleName} : ${[...args]}\n`)
-
             writeStreamError.write(`${date} : ${moduleName} : ${[...args]}\n`)
             console.error(bgRed(`${moduleName}:`), ...args);
         }
