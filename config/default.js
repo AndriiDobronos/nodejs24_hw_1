af90905164dfdde313a8f6890b4818e801e81cc1
Andrii Dobronos <andriidobronos@gmail.com>
2024-05-12 10:19:28 +0300

    created simple server

--- config/default.js
+++ config/default.js
@@ -1,8 +1,3 @@
-//const isProd = process.env.NODE_ENV === 'production';
-//const testValue: isProd ? 'this-is-prod' : process.env.TEST_VALUE
-//const isProd = process.env.NODE_ENV === 'production';
-//const testValue: isProd ? 'this-is-prod' : process.env.TEST_VALUE
-
 module.exports = {
     colorsEnabled: process.env.COLORS_ENABLED || 0,
     logLevel: process.env.LOG_LEVEL || 'warn',
